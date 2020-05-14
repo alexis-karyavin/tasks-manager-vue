43f3515d0175ec4e3a2ddc905240c3e9c04761c0
alexis-karyavin <alexis-karyavin@yandex.ru>
2020-05-14 23:33:41 +0300

    Initial commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">Tasks Manager</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <router-link
+              tag="b-nav-item"
+              to="/"
+              exact
+          >
+            Create
+          </router-link>
+          <router-link
+            tag="b-nav-item"
+            to="/list"
+            exact
+          >
+            List
+          </router-link>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "NavBar"
+  }
+</script>
+
+<style scoped>
+
+</style>
