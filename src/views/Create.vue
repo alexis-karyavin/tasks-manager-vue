b531dc5eef7481fe3508f053be25271ff7697012
alexis-karyavin <alexis-karyavin@yandex.ru>
2020-05-14 23:44:43 +0300

    Add form

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,12 +1,45 @@
 <template>
   <div>
-    <h1>Create </h1>
+    <h1>Create Task</h1>
+    <b-form @submit="onSubmit" @reset="onReset">
+      <b-form-group
+          id="input-group-1"
+          label="Title"
+          label-for="input-1"
+          description=""
+      >
+        <b-form-input
+            id="input-1"
+            v-model="form.title"
+            type="text"
+            required
+            placeholder="Enter title"
+        ></b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'Create'
+  name: 'Create',
+  data() {
+    return {
+      form: {
+        title: '',
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit')
+    },
+    onReset() {
+      console.log('reset')
+    }
+  }
 }
 </script>
