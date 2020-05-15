cf9504550f53ffe3ef0e0483642a0a7393f237b6
Alexei Karyavin <alexis-karyavin@yandex.ru>
2020-05-15 18:00:40 +0300

    Create template table

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,10 +1,27 @@
 <template>
-  <h1>List</h1>
+  <div class="container-list">
+    <h1>List</h1>
+    <b-table striped hover :items="tasks" v-if="tasks.length"></b-table>
+    <p v-else>No Tasks</p>
+  </div>
 </template>
 
 <script>
   export default {
-    name: "List"
+    name: "List",
+    data() {
+      return {
+
+      }
+    },
+    computed: {
+      tasks() {
+        let tasks = this.$store.getters.tasks;
+        // tasks = {id, ...tasks};
+
+        return tasks;
+      },
+    }
   }
 </script>
 
