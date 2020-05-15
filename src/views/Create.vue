cc9ada165932cec37ce862b65012993a25e856af
Alexei Karyavin <alexis-karyavin@yandex.ru>
2020-05-15 16:25:01 +0300

    Add Form create task

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Create Task</h1>
-    <b-form @submit="onSubmit" @reset="onReset">
+    <b-form @submit.prevent="onSubmit" @reset.prevent="onReset">
       <b-form-group
           id="input-group-1"
           label="Title"
@@ -13,8 +13,31 @@
             v-model="form.title"
             type="text"
             required
+            :state="!!form.title"
             placeholder="Enter title"
         ></b-form-input>
+        <b-form-invalid-feedback :state="(!!form.title)">Please enter title</b-form-invalid-feedback>
+      </b-form-group>
+      <b-form-group label="Tags">
+        <b-form-tags
+            input-id="tags-basic"
+            v-model="form.tags"
+            tag-variant="primary"
+        ></b-form-tags>
+      </b-form-group>
+      <b-form-group label="Description">
+        <b-form-textarea
+            v-if="form.description"
+            id="textarea"
+            v-model="form.description.text"
+            :state="validateTextArea"
+            placeholder="Enter something..."
+            no-resize
+        ></b-form-textarea>
+        <span class="countSymbols">{{ form.description.text.length }}/{{ form.description.countSym }}</span>
+      </b-form-group>
+      <b-form-group label="Deadline">
+        <b-form-datepicker v-model="form.deadline"></b-form-datepicker>
       </b-form-group>
       <b-button type="submit" variant="primary">Submit</b-button>
       <b-button type="reset" variant="danger">Reset</b-button>
@@ -30,16 +53,39 @@ export default {
     return {
       form: {
         title: '',
+        tags: [],
+        description: {
+          countSym: 2048,
+          text: ''
+        },
+        deadline: this.getCurrentDate()
       }
     }
   },
+  computed: {
+    validateTextArea: function() {
+      return this.form.description.text.length < this.form.description.countSym && this.form.description.text.length !== 0;
+    },
+  },
   methods: {
     onSubmit() {
-      console.log('submit')
+      console.log(this.form)
     },
     onReset() {
       console.log('reset')
+    },
+    getCurrentDate() {
+      const today = new Date();
+      const dd = String(today.getDate()).padStart(2, '0');
+      const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+      const yyyy = today.getFullYear();
+      return `${yyyy}-${mm}-${dd}`
     }
   }
 }
 </script>
+<style scoped lang="scss">
+  .countSymbols {
+
+  }
+</style>
