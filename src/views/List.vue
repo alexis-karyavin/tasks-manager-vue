6e3cbd1eac4bff53569d5f927f8f2844d7ba15bf
Alexei Karyavin <alexis-karyavin@yandex.ru>
2020-05-16 23:06:45 +0300

    button click route

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="container-list">
     <h1>List</h1>
-    <b-table striped hover :items="tasks" v-if="tasks.length"></b-table>
+    <b-table
+      striped
+      hover
+      :items="tasks"
+      :fields="fields"
+      v-if="tasks.length"
+    >
+      <template v-slot:cell(open)="row">
+        <b-button
+          variant="success"
+          size="sm"
+          @click="openTask(row.item.id)"
+        >Open</b-button>
+      </template>
+    </b-table>
     <p v-else>No Tasks</p>
   </div>
 </template>
@@ -9,18 +23,29 @@
 <script>
   export default {
     name: "List",
-    data() {
-      return {
-
-      }
-    },
     computed: {
       tasks() {
         let tasks = this.$store.getters.tasks;
-        // tasks = {id, ...tasks};
-
+        tasks = tasks.map(item => {
+          return {
+            ...item,
+            tags: item.tags.join(', '),
+          }
+        });
         return tasks;
       },
+      fields() {
+        const tasks = this.$store.getters.tasks;
+        if(tasks.length > 0) {
+          let key = Object.keys(this.$store.getters.tasks[0])
+          return [...key, 'open'];
+        }
+      }
+    },
+    methods: {
+      openTask(id) {
+        this.$router.push('/task/'+ id);
+      }
     }
   }
 </script>
