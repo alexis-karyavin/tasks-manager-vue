b3fae59b046d2255bc904d64c94ce92b3a6f304e
Alexei Karyavin <alexis-karyavin@yandex.ru>
2020-05-17 00:08:54 +0300

    save function

--- src/store/index.js
+++ src/store/index.js
@@ -10,17 +10,31 @@ export default new Vuex.Store({
   mutations: {
     createTask(state, task) {
       state.tasks.push({id: state.tasks.length + 1, ...task});
-
+      localStorage.setItem('tasks', JSON.stringify(state.tasks));
+    },
+    deleteTask(state, task) {
+      const id = state.tasks.findIndex(item => item.id === task.id);
+      state.tasks.splice(id, 1);
+    },
+    updateTask(state, task) {
+      // state.tasks[task.id] = {...task}
       localStorage.setItem('tasks', JSON.stringify(state.tasks));
     }
   },
   actions: {
     createTask({commit}, task) {
       commit('createTask', task);
+    },
+    deleteTask({commit}, task) {
+      commit('deleteTask', task);
+    },
+    updateTask({commit}, task) {
+      commit('updateTask', task);
     }
   },
   getters: {
-    tasks: s => s.tasks
+    tasks: s => s.tasks,
+    taskById: s => id => s.tasks.find(t => t.id === id)
   },
   modules: {
   }
--- src/views/Task.vue
+++ src/views/Task.vue
@@ -1,13 +1,51 @@
 <template>
-  <h1>Task</h1>
+  <div v-if="!!task">
+    <h1>{{task.title}}</h1>
+    <b-form @submit.prevent="onSave">
+      <b-form-group label="Description">
+        <b-form-textarea
+                id="textarea-default"
+                placeholder="Default textarea"
+                v-model="task.description"
+                :state="validateTextArea"
+        ></b-form-textarea>
+        <span class="countSymbols">{{ task.description.length }}/{{ maxCountSymbols }}</span>
+      </b-form-group>
+      <b-form-group label="Deadline">
+        <b-form-datepicker v-model="task.deadline"></b-form-datepicker>
+      </b-form-group>
+      <b-button type="submit" variant="success" class="mr-3">Save</b-button>
+      <b-button type="submit" variant="danger">Close Task</b-button>
+    </b-form>
+  </div>
+  <h1 v-else> Not Found Task</h1>
 </template>
 
 <script>
   export default {
-    name: "Task"
+    name: "Task",
+    data() {
+      return {
+        maxCountSymbols: 2048,
+      }
+    },
+    computed: {
+      task() {
+        return this.$store.getters.taskById(+this.$route.params.id);
+      },
+      validateTextArea: function() {
+        return this.task.description.length < this.maxCountSymbols && this.task.description.length !== 0;
+      },
+    },
+    methods: {
+      onSave() {
+        this.$store.dispatch('updateTask', this.task);
+        this.$router.push('/list');
+      }
+    }
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
 </style>
